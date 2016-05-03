89ebc7b2e4b02eee76e325c948277f8e6e895d13
Martin Page <wookoouk@gmail.com>
2016-05-03 11:17:03 +0100

    init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func readLine(path string) string {
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	line := 0
+	pos := 1
+
+	lastGood := 0
+
+	for scanner.Scan() {
+
+		line++
+
+		text := scanner.Text()
+
+		switch pos {
+		case 1:
+			if (!strings.HasPrefix(text, "@")) {
+				return "scan failed at line " + strconv.Itoa(line) + " should be @ but got " + string(text[0]) + ", last good read ended at " + strconv.Itoa(lastGood*4)
+			}
+
+		case 3:
+
+			if (!strings.HasPrefix(text, "+")) {
+				return "scan failed at line " + strconv.Itoa(line) + " should be + but got " + string(text[0]) + ", last good read ended at " + strconv.Itoa(lastGood*4)
+			}
+		}
+
+
+
+		if (pos == 4) {
+			lastGood++
+			pos = 1
+		} else {
+			pos++
+		}
+	}
+	return "unknown out"
+}
+
+func main() {
+	argsWithoutProg := os.Args[1]
+	println(readLine(argsWithoutProg));
+}
